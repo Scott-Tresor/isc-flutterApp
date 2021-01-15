8fd3aa4d7bc0c7eafdd7580f887be78ae15df9d1
tresor <scotttresor@gmail.com>
2021-01-15 23:38:30 +0200

    (feat): add register and login component

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/ui/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ISC',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text('Welcome', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30
+                  ),),
+                  SizedBox(height: 20,),
+                  Text(
+                    'Automatic identity verification wich enable you to verify your identity',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[700]
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('')
+                  )
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  MaterialButton(
+                    onPressed: (){},
+                    minWidth: double.infinity,
+                    height: 45,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      'Connexion',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    padding: EdgeInsets.only(top: 0, bottom: 0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border(
+                        bottom: BorderSide(color: Colors.black),
+                        top: BorderSide(color: Colors.black),
+                        left: BorderSide(color: Colors.black),
+                        right: BorderSide(color: Colors.black)
+                      )
+                    ),
+                    child: MaterialButton(
+                      onPressed: (){},
+                      minWidth: double.infinity,
+                      height: 47.5,
+                      color: Colors.yellow,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)
+                      ),
+                      child: Text(
+                        'Inscription',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
-      home: HomePage(),
     );
   }
 }
