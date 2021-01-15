29f08609f9885b7c571157c29492dac69786db40
tresor <scotttresor@gmail.com>
2021-01-15 20:48:33 +0200

    (feat): add design in application

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -14,15 +14,76 @@ class _HomePageState extends State<HomePage>
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Scott Tresor',
-          style: TextStyle(
-            color: Colors.deepOrangeAccent
-          )
-        ),
-      ),
-      body: Scaffold(
 
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            searchBox(),
+            SizedBox(height: 40,),
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20)
+                ),
+                color: Colors.white10
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("For you", style: TextStyle(
+                    color: Colors.white30,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  SizedBox(height: 20,),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(
+                        color: Colors.lightBlueAccent
+                      ))
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Recommanded', style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15
+                            ),),
+                            Container(
+                              width: 50,
+                              padding: EdgeInsets.only(bottom: 10),
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(
+                                  color: Colors.orange,
+                                  width: 3
+                                ))
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(width: 20,),
+                        Text('Likes', style: TextStyle(
+                          color: Colors.greenAccent,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15
+                        ),)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavyBar(
         animationDuration: Duration(milliseconds: 1000),
@@ -58,6 +119,58 @@ class _HomePageState extends State<HomePage>
               activeColor: Colors.greenAccent,
               inactiveColor: Colors.black
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Best place to \nFind awesome photos',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.black54,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(height: 30,),
+          Container(
+            height: 50,
+            padding: EdgeInsets.only(left: 20, right: 3, top: 3, bottom: 3),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.black38
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search student",
+                      hintStyle: TextStyle(color: Colors.white),
+                      border: InputBorder.none
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: (){},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  height: double.infinity,
+                  minWidth: 50,
+                  elevation: 0,
+                  color: Colors.amber,
+                  child: Icon(Icons.search, color: Colors.white,),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
