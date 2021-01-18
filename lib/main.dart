262f731718fac0c2d2ceb6c032974676f2c83f8e
tresor <scotttresor@gmail.com>
2021-01-18 23:51:55 +0200

    (feat): add onBoarding in application

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/users/login.dart';
-import 'package:flutter_app/users/singup.dart';
+import 'package:flutter_app/homePage.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    )
+    MyApp()
   );
 }
 
-class SplashScreen extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text('Welcome', style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30
-                  ),),
-                  SizedBox(height: 20,),
-                  Text(
-                    'Automatic identity verification wich enable you to verify your identity',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey[700]
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('')
-                  )
-                ),
-              ),
-              Column(
-                children: <Widget>[
-                  MaterialButton(
-                    animationDuration: Duration(milliseconds: 1000),
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-                    },
-                    minWidth: double.infinity,
-                    height: 45,
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.black
-                      ),
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                    child: Text(
-                      'Connexion',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  Container(
-                    padding: EdgeInsets.only(top: 0, bottom: 0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black)
-                      )
-                    ),
-                    child: MaterialButton(
-                      animationDuration: Duration(milliseconds: 1000),
-                      onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => SingUpPage()));
-                      },
-                      minWidth: double.infinity,
-                      height: 47.5,
-                      color: Colors.yellow,
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)
-                      ),
-                      child: Text(
-                        'Inscription',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
+    return MaterialApp(
+      title: "ISC LUBUMBASHI",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.redAccent,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
           ),
-        ),
+          headline5: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white60
+          ),
+          bodyText1: TextStyle(
+            fontSize: 14.0,
+            color: Colors.white
+          ),
+          bodyText2: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white
+          )
+        )
       ),
+      home: HomePageScreen(),
     );
   }
 }
