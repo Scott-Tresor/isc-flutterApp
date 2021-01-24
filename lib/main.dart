c63fb24e32286027c9903aa98948f16ec030b1d8
tresor <scotttresor@gmail.com>
2021-01-24 21:09:12 +0200

    (feat):add splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/homePage.dart';
+import 'package:flutter_app/ui/FadeAnimation.dart';
 
 void main() {
   runApp(
@@ -11,34 +11,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "ISC LUBUMBASHI",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.redAccent,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontSize: 38.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-          ),
-          headline5: TextStyle(
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white60
-          ),
-          bodyText1: TextStyle(
-            fontSize: 14.0,
-            color: Colors.white
-          ),
-          bodyText2: TextStyle(
-            fontSize: 20.0,
-            color: Colors.white
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/photo2.png'),
+            fit: BoxFit.cover
           )
-        )
+        ),
+        child: Container(
+          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              colors: [
+                Colors.black.withOpacity(.7),
+                Colors.black.withOpacity(.6),
+                Colors.black.withOpacity(.6),
+                Colors.black.withOpacity(.3),
+              ]
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              FadeAnimation(1,
+                Text("Bienvenu sur l'application officiel de l'institut  superieur de commerce",
+                  style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold, height: 1.0),
+                )
+              ),
+              SizedBox(height: 30,),
+              FadeAnimation(1.3,
+                Text("Une application de gestion des etudiants",
+                  style: TextStyle(color: Colors.white.withOpacity(.7), fontSize: 23, fontWeight: FontWeight.w500),
+                )
+              ),
+              SizedBox(height: 150,),
+              InkWell(
+                onTap: (){},
+              )
+            ],
+          ),
+        ),
       ),
-      home: HomePageScreen(),
     );
   }
 }
